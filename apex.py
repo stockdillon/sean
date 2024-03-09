9d512b519defd696349cf1431cad1ff3fe349640
Dillon Stock <dillon.w.stock@gmail.com>
2024-03-09 11:39:02 -0500

    clean up url

--- apex.py
+++ apex.py
@@ -9,13 +9,13 @@ APEX_API_KEY = 'APEX_API_KEY'
 
 class ApexQuery:
     """
-    order_date
-    date	
-    https://app.apextrading.com/api/v1/shipping-orders?order_date=2021-04-20 
-
     order_date_from
     date	
     https://app.apextrading.com/api/v1/shipping-orders?order_date_from=2021-04-20
+
+    order_date_to
+    date	
+    https://app.apextrading.com/api/v1/shipping-orders?order_date_to=2021-04-20
     """
 
     def __init__(self, start: date, end: date) -> None:
@@ -25,7 +25,7 @@ class ApexQuery:
 
 class Apex:
     def __init__(self) -> None:
-        self.url = "https://app.apextrading.com/api/v1/shipping-orders"
+        self.base_url = "https://app.apextrading.com/api/v1"
         api_key = os.environ.get(APEX_API_KEY)
         if (not api_key):
             raise ValueError(
@@ -37,6 +37,7 @@ class Apex:
         print('created apex client')
 
     def get_shipping_orders(self, query: ApexQuery = None) -> List[object]:
-        response = requests.get(self.url, headers=self.headers)
+        url = os.path.join(self.base_url, 'shipping-orders')
+        response = requests.get(url, headers=self.headers)
         response_object = json.loads(response.text)
         return response_object
